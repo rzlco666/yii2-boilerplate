c8691cd38dcbc48e4f333978804ebd326972fd33
Rizal <syahrizalhanif@gmail.com>
2022-11-06 12:55:22 +0700

    Custom migrate
    To split user frontend and backend

--- console/migrations/m130524_201442_init.php
+++ console/migrations/m130524_201442_init.php
@@ -24,10 +24,24 @@ class m130524_201442_init extends Migration
             'created_at' => $this->integer()->notNull(),
             'updated_at' => $this->integer()->notNull(),
         ], $tableOptions);
+
+        $this->createTable('{{%admin}}', [
+            'id' => $this->primaryKey(),
+            'username' => $this->string()->notNull()->unique(),
+            'auth_key' => $this->string(32)->notNull(),
+            'password_hash' => $this->string()->notNull(),
+            'password_reset_token' => $this->string()->unique(),
+            'email' => $this->string()->notNull()->unique(),
+
+            'status' => $this->smallInteger()->notNull()->defaultValue(10),
+            'created_at' => $this->integer()->notNull(),
+            'updated_at' => $this->integer()->notNull(),
+        ], $tableOptions);
     }
 
     public function down()
     {
         $this->dropTable('{{%user}}');
+        $this->dropTable('{{%admin}}');
     }
 }
--- console/migrations/m190124_110200_add_verification_token_column_to_user_table.php
+++ console/migrations/m190124_110200_add_verification_token_column_to_user_table.php
@@ -7,10 +7,12 @@ class m190124_110200_add_verification_token_column_to_user_table extends Migrati
     public function up()
     {
         $this->addColumn('{{%user}}', 'verification_token', $this->string()->defaultValue(null));
+        $this->addColumn('{{%admin}}', 'verification_token', $this->string()->defaultValue(null));
     }
 
     public function down()
     {
         $this->dropColumn('{{%user}}', 'verification_token');
+        $this->dropColumn('{{%admin}}', 'verification_token');
     }
 }
